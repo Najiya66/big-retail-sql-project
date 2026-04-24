2368b9fe9f70a6b793bb8155d81c3c32221e3348
Najiya66 <najiya66nasrin@gmail.com>
2026-04-25 00:02:04 +0530

    Create analysis.sql

---
+++ sql/analysis.sql
@@ -0,0 +1,5 @@
+SELECT c.name, SUM(o.quantity * p.price) AS total_spent
+FROM orders o
+JOIN customers c ON o.customer_id = c.customer_id
+JOIN products p ON o.product_id = p.product_id
+GROUP BY c.name;
